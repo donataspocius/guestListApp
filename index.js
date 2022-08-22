a6b6ef60b793802cb2da384e00c2989b916db5f0
Donatas Pocius <pocius.coding@gmail.com>
2022-08-22 23:20:29 +0300

    code v1

--- index.js
+++ index.js
@@ -1,49 +1,36 @@
-"use strict";
-
-let guestInput = document.querySelector(".input-data");
-let inputField = document.querySelector(".input-field");
 let allBtns = document.querySelectorAll(".btns");
-let output = document.querySelector(".output");
-let guestName = null;
+let inputField = document.querySelector(".input-field");
+
 let guestList = [];
-
-guestInput.addEventListener("input", function (e) {
-  guestName = e.target.value;
-});
-
-let pEl = document.createElement("p");
-output.append(pEl);
 
 allBtns.forEach((btn) =>
   btn.addEventListener("click", (e) => {
-    console.log(e.target.name);
+    let guestName = inputField.value;
     switch (e.target.name) {
       case "guestEnd": {
-        guestList.push(guestName);
-        inputField.value = "";
-        break;
+        if (inputField.value) {
+          guestList.push(guestName);
+          break;
+        }
       }
       case "guestStart": {
-        guestList.unshift(guestName);
-        inputField.value = "";
-        break;
+        if (inputField.value) {
+          guestList.unshift(guestName);
+          break;
+        }
       }
       case "removeFirst": {
         guestList.shift(guestName);
-        inputField.value = "";
         break;
       }
       case "removeLast": {
         guestList.pop(guestName);
-        inputField.value = "";
         break;
       }
       case "reverse": {
         guestList.reverse();
-        inputField.value = "";
         break;
       }
     }
-    pEl.textContent = guestList;
   })
 );
