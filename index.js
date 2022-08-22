8ec3ff4fa3b5f4451da52cbe4e03255fb776ffb2
Donatas Pocius <pocius.coding@gmail.com>
2022-08-23 00:26:27 +0300

    code v2 / why deletes list items one by one IF INPUT EMPTY?

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let allBtns = document.querySelectorAll(".btns");
 let inputField = document.querySelector(".input-field");
+let guestsContainer = document.querySelector(".guests");
 
 let guestList = [];
 
 allBtns.forEach((btn) =>
   btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(e.target.name);
     let guestName = inputField.value;
     switch (e.target.name) {
       case "guestEnd": {
@@ -32,5 +35,23 @@ allBtns.forEach((btn) =>
         break;
       }
     }
+    renderGuestList(guestList);
+    inputField.value = "";
   })
 );
+
+function renderGuestList(list) {
+  if (document.querySelector(".guests ol")) {
+    document.querySelector(".guests ol").remove();
+  }
+
+  let createList = document.createElement("ol");
+
+  list.forEach((el) => {
+    let guest = document.createElement("li");
+    guest.textContent = el;
+    createList.appendChild(guest);
+  });
+
+  guestsContainer.append(createList);
+}
