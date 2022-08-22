e7cb5efbf4a647636fcae57139f55980210c91fb
Donatas Pocius <pocius.coding@gmail.com>
2022-08-22 19:46:20 +0300

    1st review

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 "use strict";
+
+let guestInput = document.querySelector(".input-data");
+let inputField = document.querySelector(".input-field");
+let allBtns = document.querySelectorAll(".btns");
+let output = document.querySelector(".output");
+let guestName = null;
+let guestList = [];
+
+guestInput.addEventListener("input", function (e) {
+  guestName = e.target.value;
+});
+
+let pEl = document.createElement("p");
+output.append(pEl);
+
+allBtns.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    switch (e.target.name) {
+      case "guestEnd": {
+        guestList.push(guestName);
+        inputField.value = "";
+        break;
+      }
+      case "guestStart": {
+        guestList.unshift(guestName);
+        inputField.value = "";
+        break;
+      }
+      case "removeFirst": {
+        guestList.shift(guestName);
+        inputField.value = "";
+        break;
+      }
+      case "removeLast": {
+        guestList.pop(guestName);
+        inputField.value = "";
+        break;
+      }
+      case "reverse": {
+        guestList.reverse();
+        inputField.value = "";
+        break;
+      }
+    }
+    pEl.textContent = guestList;
+  })
+);
